8491f507bf80551deeccee1213b7d04d0226aa58
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-08 16:53:58 +0330

    learn choices for CharField

--- store/models.py
+++ store/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-# Products
-# name
-# description
-# price
-# add date and time
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -22,3 +17,33 @@ class Customer(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p'
+    ORDER_STATUS_UNPAID = 'u'
+    ORDER_STATUS_CANCELED = 'c'
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID,'Paid'),
+        (ORDER_STATUS_UNPAID,'Unpaid'),
+        (ORDER_STATUS_CANCELED,'Canceled'),
+    ]
+    # customer
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+
+
+class Comment(models.Model):
+    COMMENT_STATUS_WAITING = 'w'
+    COMMENT_STATUS_APPROVED = 'a'
+    COMMENT_STATUS_NOT_APPROVED = 'na'
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_WAITING, 'Waiting'),
+        (COMMENT_STATUS_APPROVED, 'Approved'),
+        (COMMENT_STATUS_NOT_APPROVED, 'Not Approved'),
+    ]
+    # product
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITING)
