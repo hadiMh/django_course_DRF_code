7954072c1dd3d865372fdc02f776f7617cd5f5a9
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-18 05:24:10 +0330

    learn migrate and sqlite database plugin

--- store/models.py
+++ store/models.py
@@ -58,7 +58,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
     quantity = models.PositiveSmallIntegerField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         unique_together = [['order', 'product']]
