d2ef013896fbe8cd9a34f87d9a1261c76a27240f
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-24 12:46:37 +0330

    learn admin computed fields

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,16 @@ from .models import Category, Order, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'inventory', 'unit_price']
+    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status']
     list_per_page = 10
     list_editable = ['unit_price']
+
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        if product.inventory > 50:
+            return 'High'
+        return 'Medium'
 
 
 @admin.register(Order)
@@ -16,5 +23,4 @@ class OrderAdmin(admin.ModelAdmin):
     list_per_page = 10
     ordering = ['-datetime_created']
 
-# admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
