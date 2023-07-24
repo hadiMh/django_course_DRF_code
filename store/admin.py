72c8cb4646b0ebd7e1c5b0ef2b389663ac2ded88
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-24 12:36:06 +0330

    learn list_display, editable and ordering

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Category, Order, Product
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'inventory', 'unit_price']
+    list_per_page = 10
+    list_editable = ['unit_price']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'status', 'datetime_created']
+    list_editable = ['status']
+    list_per_page = 10
+    ordering = ['-datetime_created']
+
+# admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
     description = models.CharField(max_length=500, blank=True)
     top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self):
+        return self.title
+
+
 
 class Discount(models.Model):
     discount = models.FloatField()
@@ -24,6 +28,10 @@ class Product(models.Model):
     datetime_modified = models.DateTimeField(auto_now=True)
     discounts = models.ManyToManyField(Discount, blank=True)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
@@ -31,6 +39,9 @@ class Customer(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Address(models.Model):
