0f8827adcf6efdbb6240330bc9b5512e196216ce
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-24 11:39:05 +0330

    sort imports and clean them

--- store/views.py
+++ store/views.py
@@ -1,22 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.db.models import F, ExpressionWrapper, DecimalField
 from django.db import transaction, connection
+from django.db.models import F, ExpressionWrapper, DecimalField
+from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Category, Comment, Product, Customer, OrderItem, Order
 
 
 def show_data(request):
-    cursor = connection.cursor()
-    # cursor.execute('')
-    # cursor.close()
-
-    # with connection.cursor() as cursor:
-    #     cursor.execute('')
-
-    # Product.objects.raw('SELECT is, unit_price FROM store_product')
-
-    # cursor.callproc('some_proc', 1, '2', 'hello')
-
     return render(request, 'hello.html')
 
