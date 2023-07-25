a13dbf7b7da7987bc950c130f6d71e77533b656b
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-25 11:51:57 +0330

    learn how to use validators on fields

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.query import QuerySet
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -26,7 +26,7 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     discounts = models.ManyToManyField(Discount, blank=True)
