155ed83f18f240839d2ec01f79d248f8ad6f7711
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-25 08:36:34 +0330

    learn select_related on admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Category, Order, Product
+from .models import Category, Comment, Order, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status']
+    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status', 'product_category']
     list_per_page = 10
     list_editable = ['unit_price']
+    list_select_related = ['category']
 
     def inventory_status(self, product):
         if product.inventory < 10:
@@ -14,6 +15,16 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory > 50:
             return 'High'
         return 'Medium'
+    
+    def product_category(self, product):
+        return product.category.title
+    
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'status', ]
+    list_editable = ['status']
+    list_per_page = 10
 
 
 @admin.register(Order)
