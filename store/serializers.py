6106268a6d0660b2fb276bfd697a0579bddbbcab
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 04:30:28 +0330

    change back the names of serializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255, source='name')
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    name = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     inventory = serializers.IntegerField()
