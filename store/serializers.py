b8a6a850d6405880ae1754f44076214f18fac5cb
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 05:17:47 +0330

    learn SerializerMethodField

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 
@@ -5,4 +6,8 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
+    
+    def get_unit_price_after_tax(self, product):
+        return round(product.unit_price * Decimal(1.09), 2)
