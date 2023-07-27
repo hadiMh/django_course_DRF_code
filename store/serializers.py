591e79549fdb8c74ac01906fbc9cf13767b410dc
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 04:29:55 +0330

    use source in serializer fields

--- store/serializers.py
+++ store/serializers.py
@@ -3,5 +3,6 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    title = serializers.CharField(max_length=255, source='name')
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    inventory = serializers.IntegerField()
