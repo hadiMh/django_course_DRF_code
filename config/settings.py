218144d3c07d580f988aa4cb3d31ca9fa85a18bb
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 05:03:54 +0330

    add dtf settings to not convert decimal to string

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+}
