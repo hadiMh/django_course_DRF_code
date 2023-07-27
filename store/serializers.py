2519dbc4b1312355ecc5b3f54dc4dddd42538194
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 05:36:58 +0330

    use CategorySerializer inside ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -4,13 +4,18 @@ from rest_framework import serializers
 from .models import Category
 
 
+class CategorySerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField(max_length=500)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
-    category = serializers.StringRelatedField()
+    category = CategorySerializer()
     
     def get_unit_price_after_tax(self, product):
         return round(product.unit_price * Decimal(1.09), 2)
