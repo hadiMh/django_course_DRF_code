8ca2d9853165965aafe34a61e193402e1f8bcf6a
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-27 03:56:25 +0330

    learn BrowsableAPI and simple get request DRF

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('products/', views.product_list),
+    path('products/<int:id>/', views.product_detail),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def product_list(request):
+    return Response('Hello')
 
 
+@api_view()
+def product_detail(request, id):
+    return Response(id)
