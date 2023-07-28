7e1c2a50926cfc3651aa89632b5dcf29638d343b
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-28 09:49:21 +0330

    use SimpleRouter for routing

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
 from . import views
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('categories/', views.CategoryList.as_view()),
-    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
-]
+router = SimpleRouter()
+router.register('products', views.ProductViewSet, basename='product')
+router.register('categories', views.CategoryViewSet, basename='category')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetail.as_view()),
+#     path('categories/', views.CategoryList.as_view()),
+#     path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
+# ]
