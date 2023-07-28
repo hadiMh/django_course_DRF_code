e2fa8203ab4cb1d7cd2cc62cc14dd961f2acdf89
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-28 09:27:11 +0330

    detail view to RetrieveUpdateDestroyAPIView

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -18,24 +18,9 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
 
 
-class ProductDetail(APIView):
-    def get(self, request, pk):
-        product = get_object_or_404(
-            Product.objects.select_related('category'),
-            pk=pk,
-        )
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        product = get_object_or_404(
-            Product.objects.select_related('category'),
-            pk=pk,
-        )
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.select_related('category').all()
     
     def delete(self, request, pk):
         product = get_object_or_404(
@@ -53,18 +38,9 @@ class CategoryList(ListCreateAPIView):
     queryset = Category.objects.prefetch_related('products').all()
 
 
-class CategoryDetail(APIView):
-    def get(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class CategoryDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.prefetch_related('products')
     
     def delete(self, request, pk):
         category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
