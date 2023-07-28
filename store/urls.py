b51bfa90d610e098ddb723f355d0b43798764315
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-28 09:52:45 +0330

    use DefaultRouter

--- store/urls.py
+++ store/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
 from . import views
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('categories', views.CategoryViewSet, basename='category')
 
 urlpatterns = router.urls
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-# urlpatterns = [
-#     path('products/', views.ProductList.as_view()),
-#     path('products/<int:pk>/', views.ProductDetail.as_view()),
-#     path('categories/', views.CategoryList.as_view()),
-#     path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
-# ]
