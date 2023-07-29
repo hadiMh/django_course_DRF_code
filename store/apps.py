5a65b499d6ce58592065350834f853d5e39ba927
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-29 11:53:33 +0330

    use signals for customer profile

--- store/apps.py
+++ store/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+    def ready(self) -> None:
+        from . import signals
