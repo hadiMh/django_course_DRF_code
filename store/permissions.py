c226230ee297f6b250b5fdc50ae1389e030a3d3c
hadiMh <m.hadi.hajihosseini@gmail.com>
2023-07-29 16:46:03 +0330

    use SAFE_METHODS in drf

--- store/permissions.py
+++ store/permissions.py
@@ -3,5 +3,6 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.method == 'GET' or (request.user and request.user.is_staff))
-        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
